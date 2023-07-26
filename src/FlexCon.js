ef1f64beb791824e03f0efe871d44c5c644c7da9
G4GA <angel.garcia9106@alumnos.udg.mx>
2023-07-26 10:07:59 -0600

    a

--- src/FlexCon.js
+++ src/FlexCon.js
@@ -5,13 +5,10 @@ import "./FlexCon.css";
 export default function FlexCon() {
   return (
     <div className="FlexCon">
-      <Picture
-        imgSrc={"./elizabeth-bday/img1love.jpg"}
-        message={"Así yo cuando te veo jiji"}
-      />
+      <Picture imgSrc={"img1love.jpg"} message={"Así yo cuando te veo jiji"} />
       <Greeting />
       <Picture
-        imgSrc={"./elizabeth-bday/img2love.jpg"}
+        imgSrc={"img2love.jpg"}
         message={"Así yo cuando te veo x2 jiji"}
       />
     </div>
